45ec33e320598744fdfe244098f89911b6bffb8a
Spider-Webs <dyko615@naver.com>
2022-07-29 22:18:30 +0900

    회원서비스 클래스구혀

--- core (2)/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core (2)/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 //회원서비스 구현 클래스
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
@@ -15,4 +19,9 @@ public class MemberServiceImpl implements MemberService{
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
+    }
+
 }
