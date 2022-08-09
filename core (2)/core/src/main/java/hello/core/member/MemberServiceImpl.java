66e07e9442a0cb62fd4d9ccd8c9d275defb7b006
Spider-Webs <dyko615@naver.com>
2022-08-10 01:02:44 +0900

    테스트

--- core (2)/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core (2)/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 //회원서비스 구현 클래스
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- core (2)/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core (2)/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 //회원저장구현 클래스(DB 사용 아닌 메모리로 저장)
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
